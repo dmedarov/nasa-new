27e3db4159c7224e33c079c5af609c47bc8f5f2b
Damian Medarov <d.medarov@yahoo.com>
2022-09-16 23:35:02 +0300

    Initial Commit

---
+++ NASA New/NASA_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_NewApp.swift
+//  NASA New
+//
+//  Created by Damian Medarov on 16.09.22.
+//
+
+import SwiftUI
+
+@main
+struct NASA_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
